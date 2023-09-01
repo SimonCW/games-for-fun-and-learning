3362518dcf192e1dabeb9063693d13f00b3c7fb8
Simon Weiß <weiss.simon@outlook.com>
2023-09-01 22:28:08 +0200

    Implement reversible player cycle

--- uno/uno/uno.py
+++ uno/uno/uno.py
@@ -1,18 +1,21 @@
 from itertools import product
 from dataclasses import dataclass
 from random import shuffle
-from enum import Enum
+from typing import Iterable
+
 
 N_INITAL_CARDS = 7
 
-Order = Enum("Order", ["NORMAL", "REVERSED"])
+
 @dataclass
 class Card:
     face: int | str
     color: str
 
+
 Deck = list[Card]
 Hand = list[Card]
+
 
 @dataclass
 class Player:
@@ -25,28 +28,40 @@ colors = "blue red yellow green".split()
 actions = "draw2 skip reverse".split()
 wilds = "draw4 wishcolor".split()
 
-def main ():
+
+def main():
     deck = build_deck()
     player_names = ["Jane", "Walther", "Jojo"]
-    players, deck = deal_cards(deck, player_names)
-    print(players, len(deck))
+    players, deck = initialize_game(deck, player_names)
+
+    pc = PlayerCycle(players)
+    for _ in range(3):
+        print(next(pc).name)
+    print("reverse")
+    pc.reverse()
+    for _ in range(3):
+        print(next(pc).name)
+
     # Game Loop
     while True:
-        pass
+        print("Game Loop")
+        break
+
 
 def build_deck() -> Deck:
     d = list(product(numbers, colors))
     d.extend(list(product(actions, colors)))
     d.extend(list(product(actions, colors)))
     d = [Card(face, color) for face, color in d]
-    wildcards = [Card(w, "wild") for w in wilds*4]
+    wildcards = [Card(w, "wild") for w in wilds * 4]
     d.extend(wildcards)
     shuffle(d)
     return d
 
-def deal_cards(deck: Deck, player_names: list[str]) -> tuple[list[Player], Deck]:
+
+def initialize_game(deck: Deck, player_names: list[str]) -> tuple[list[Player], Deck]:
     initialized = []
-    for name in player_names: 
+    for name in player_names:
         cards = []
         for _ in range(N_INITAL_CARDS):
             cards.append(deck.pop())
@@ -54,6 +69,30 @@ def deal_cards(deck: Deck, player_names: list[str]) -> tuple[list[Player], Deck]
     return initialized, deck
 
 
+class PlayerCycle:
+    """Gives the next Player. Can be reversed."""
+
+    def __init__(self, player_names: Iterable) -> None:
+        self._items = list(player_names)
+        self._pos = None
+        # 1 for normal direction, -1 for reversed
+        self._direction = 1
+
+    def __next__(self):
+        # First play in the game
+        if self._pos is None:
+            self._pos = 0 if self._direction == 1 else -1
+            return self._items[self._pos]
+
+        self._pos += self._direction
+        element = self._items[self._pos]
+        return element
+
+    def __iter__(self):
+        return self
+
+    def reverse(self):
+        self._direction *= -1
 
 
 if __name__ == "__main__":
