cc4b066869f78aa665881cc0de787c1b96e629e2
Simon Weiß <weiss.simon@outlook.com>
2023-10-13 15:27:16 +0200

    Rename for brevity

--- uno_py/uno/game.py
+++ uno_py/uno/game.py
@@ -3,14 +3,14 @@ from uno.state import GameState, Player
 
 def main():
     player_names = ["Jane", "Walther", "Jojo"]
-    game_state = GameState.from_names(player_names)
+    gs = GameState.from_names(player_names)
     # Put first open card on the pile
-    game_state.community_cards.flip_first_card()
+    gs.community_cards.flip_first_card()
     # TODO: Shouldn't have to access private attribute _pile here.
     print(
         f"""--- Starting Game ---
     Players: {player_names}
-    Top of pile: {game_state.community_cards.top_card}
+    Top of pile: {gs.community_cards.top_card}
     """
     )
     round = 1
@@ -18,53 +18,53 @@ def main():
         print(f"--- Round {round} ---")
         # TODO: Sth. with the state of the pile is wrong. The top card
         # doesn't update properly.
-        print(f"Top card: {game_state.community_cards.top_card}")
-        match game_state.community_cards.top_card.face:
+        print(f"Top card: {gs.community_cards.top_card}")
+        match gs.community_cards.top_card.face:
             case "reverse":
                 print("🔄, Reversing play direction")
-                game_state.player_cycle.reverse()
+                gs.player_cycle.reverse()
             case "skip":
-                skipped: Player = next(game_state.player_cycle)  # type: ignore
+                skipped: Player = next(gs.player_cycle)  # type: ignore
                 print(f"🛑, Skipping player: {skipped.name}")
-        up: Player = next(game_state.player_cycle)  # type: ignore
+        up: Player = next(gs.player_cycle)  # type: ignore
         print(f"Player {up} is up")
-        match game_state.community_cards.top_card.face:
+        match gs.community_cards.top_card.face:
             case "draw2":
                 to_draw = [
-                    game_state.community_cards.draw(),
-                    game_state.community_cards.draw(),
+                    gs.community_cards.draw(),
+                    gs.community_cards.draw(),
                 ]
                 print(f"🤷, you have to draw 2: {to_draw}")
                 up.hand.extend(to_draw)
             case "draw4":
                 to_draw = [
-                    game_state.community_cards.draw(),
-                    game_state.community_cards.draw(),
-                    game_state.community_cards.draw(),
-                    game_state.community_cards.draw(),
+                    gs.community_cards.draw(),
+                    gs.community_cards.draw(),
+                    gs.community_cards.draw(),
+                    gs.community_cards.draw(),
                 ]
                 print(f"🤯, you have to draw 4: {to_draw}")
                 up.hand.extend(to_draw)
             case _:
                 print("No action to take")
         card_played = up.strategy_random(
-            top_card=game_state.community_cards._pile[0]  # type: ignore
+            top_card=gs.community_cards._pile[0]  # type: ignore
         )
         if card_played:
-            game_state.community_cards.put_to_pile(card_played)
+            gs.community_cards.put_to_pile(card_played)
         else:
-            to_draw = game_state.community_cards.draw()
+            to_draw = gs.community_cards.draw()
             print(f"☝️, You cannot play, you have to draw 1: {to_draw}")
             up.hand.extend([])
             card_played = up.strategy_random(
-                top_card=game_state.community_cards.top_card  # type: ignore
+                top_card=gs.community_cards.top_card  # type: ignore
             )
             if card_played:
-                game_state.community_cards.put_to_pile(card_played)
+                gs.community_cards.put_to_pile(card_played)
         print(f"Card played: {card_played}")
-        if game_state.check_win_condition():
+        if gs.check_win_condition():
             break
-        print(f"{game_state.community_cards._pile}")
+        print(f"{gs.community_cards._pile}")
         round += 1
 
 
