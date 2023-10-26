5a9d8252ab21a8f8d847e78c2c1a67396e06c579
Simon Weiß <weiss.simon@outlook.com>
2023-10-26 12:17:53 +0200

    Implment card types

--- uno_rs/src/state.rs
+++ uno_rs/src/state.rs
@@ -1,13 +1,36 @@
-const COLOR_SPECIALS: [&str; 3] = ["Draw Two", "Reverse", "Skip"];
+// Using these consts b/c it's the easiest way to iterate over the colors
+const COLOR_SPECIALS: [ColoredCard; 3] =
+    [ColoredCard::Skip, ColoredCard::Reverse, ColoredCard::Draw];
 const WILD_SPECIALS: [&str; 2] = ["Draw Four", "Wish Color"];
-const COLORS: [Color; 4] = [Color::Blue, Color::Green, Color::Red, Color::Yellow];
+const COLORS: [CardColor; 4] = [
+    CardColor::Blue,
+    CardColor::Green,
+    CardColor::Red,
+    CardColor::Yellow,
+];
 const VALUES: [u8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-#[derive(Debug)]
-pub struct Card {
-    color: Color,
-    face: String,
-    value: Option<u8>,
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CardColor {
+    Blue,
+    Green,
+    Red,
+    Yellow,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ColoredCard {
+    Number(u8),
+    Skip,
+    Reverse,
+    Draw,
+}
+
+#[derive(Debug, Clone)]
+pub enum Card {
+    Colored(CardColor, ColoredCard),
+    Wild,
+    WildDrawFour,
 }
 
 #[derive(Debug)]
@@ -19,15 +42,18 @@ impl Deck {
     pub fn new() -> Deck {
         let mut cards = Vec::<Card>::new();
         for color in &COLORS {
-            for value in VALUES {
-                let card = Card {
-                    color: color.clone(),
-                    value: Some(value),
-                    face: value.to_string(),
-                };
-                cards.push(card);
+            for _ in 0..2 {
+                for value in VALUES {
+                    let card = Card::Colored(color.clone(), ColoredCard::Number(value));
+                    cards.push(card);
+                }
+                for special in &COLOR_SPECIALS {
+                    let card = Card::Colored(color.clone(), special.clone());
+                    cards.push(card);
+                }
             }
         }
+
         Deck { cards }
     }
 }
@@ -35,12 +61,4 @@ impl Default for Deck {
     fn default() -> Self {
         Self::new()
     }
-}
-
-#[derive(Debug, Clone)]
-enum Color {
-    Blue,
-    Green,
-    Red,
-    Yellow,
 }
