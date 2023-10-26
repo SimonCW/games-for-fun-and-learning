40265547a642a932cb5a7fbba7373f8209d8498c
Simon Weiß <weiss.simon@outlook.com>
2023-10-26 12:17:45 +0200

    Add clippy instructions

--- uno_rs/src/main.rs
+++ uno_rs/src/main.rs
@@ -1,8 +1,11 @@
+#![warn(clippy::all)]
+#![deny(clippy::pedantic)]
+#![allow(clippy::must_use_candidate)]
 use crate::state::Deck;
 
 pub mod state;
 
 pub fn main() {
     let deck = Deck::new();
-    println!("{:?}", deck);
+    println!("{deck:?}");
 }
