81c462455e3920b9af58972f6776f4623b664714
Simon Weiß <weiss.simon@outlook.com>
2023-12-22 21:51:59 +0100

    Rename and add test for proper deck init

--- uno_py/tests/test_functional.py
+++ uno_py/tests/test_functional.py
@@ -1,7 +1,14 @@
 from uno.game import main
+from uno.state import CommunityCards
 import pytest
 
 
 @pytest.mark.parametrize("execution_number", range(100))
 def test_games_should_finish_wo_exceptions(execution_number):
     main()
+
+
+def test_deck_should_have_108_cards():
+    cc = CommunityCards.new()
+    assert len(cc._deck) == 108
+    assert len(cc._pile) == 0
